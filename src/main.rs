8b5e4310edd5bfd3f7025a2d76303b5295b80283
Èrik C. Forés <soc@erik.cat>
2022-12-13 19:31:23 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,50 @@
+use valar::http::Request;
 use valar::http::Response;
+use valar::http::Result;
 use valar::routing::Router;
 use valar::Server;
 
-struct Context {}
+struct Context {
+    version: f32,
+}
+
+#[derive(serde::Serialize)]
+struct Profile {
+    id: i32,
+}
+
+async fn homepage(request: Request<Context>) -> Result {
+    let version = request.context().version;
+
+    Response::ok()
+        .html(format!("<h1>Hello, world</h1><h2>Version: {version}</h2>"))
+        .produce()
+}
+
+async fn profile(request: Request<Context>) -> Result {
+    let id = request.parameter("id")?.parse::<i32>()?;
+
+    Response::ok()
+        .html(format!("<h1>User ID: {id}</h1>"))
+        .produce()
+}
+
+async fn json_profile(request: Request<Context>) -> Result {
+    let id = request.parameter("id")?.parse::<i32>()?;
+
+    Response::ok().json(&Profile { id })?.produce()
+}
 
 #[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
+async fn main() -> std::result::Result<(), anyhow::Error> {
     env_logger::init();
-    let context = Context {};
+
+    let context = Context { version: 1. };
     let mut router = Router::<Context>::default();
 
-    router.get("/", move |_req| async move {
-        Response::ok().body("Hello, world!").produce()
-    });
-
-    router.get("/profile/:id/create", move |_req| async move {
-        Response::ok().html("<h1>User created!</h1>").produce()
-    });
-
-    router.get("/profile/:id", move |req| async move {
-        let id = req.parameter("ids")?.parse::<i32>()?;
-
-        Response::ok()
-            .html(format!("<h1>User ID: {id}</h1>"))
-            .produce()
-    });
+    router.get("/", homepage);
+    router.get("/profile/:id", profile);
+    router.get("/profile/:id/json", json_profile);
 
     let app = Server::builder()
         .address(([127, 0, 0, 1], 3000))
